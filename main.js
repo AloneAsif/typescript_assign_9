34852f41131eff44908cb43f84d1e82b4bf9cc35
[AloneAsif] <[asifleon3@gmail.com]>
2024-03-09 14:39:06 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+// // Question no # 10      Adding Comments: // program written 2/15/2024
+// single line comment 
+/*  multi
+    line
+    comments
+*/ 
---
+++ main.ts
@@ -0,0 +1,9 @@
+
+// // Question no # 10      Adding Comments: // program written 2/15/2024
+
+// single line comment 
+
+/*  multi
+    line
+    comments
+*/
